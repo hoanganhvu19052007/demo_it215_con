6df78ed6c9fe649b3f7b1893446eb2104084a503
hoanganhvu19052007 <hoangvu2007hg@gmail.com>
2026-08-19 21:04:33 +0700

    Delete construction_api directory

--- construction_api/app/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from app.models.user import User          # noqa: F401
-from app.models.site import ConstructionSite, SiteMember  # noqa: F401
-from app.models.work_item import WorkItem  # noqa: F401
