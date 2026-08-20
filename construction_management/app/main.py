4c9cf5ca9ef52413a60b45c3de6a544d4f998a61
hoanganhvu19052007 <hoangvu2007hg@gmail.com>
2026-08-20 07:50:30 +0700

    feat(main): uncomment auth and user routers

--- construction_management/app/main.py
+++ construction_management/app/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 
-# from app.router.auth import router as auth_router
+from app.router.auth import router as auth_router
 from app.router.health import router as health_router
+from app.router.user import router as user_router
 
 # from app.router.site import router as site_router
-# from app.router.user import router as user_router
 # from app.router.work_item import router as work_item_router
 from app.core.config import settings
 from app.core.exceptions import register_exception_handlers
@@ -17,8 +17,8 @@ app = FastAPI(title="Construction Management API", debug=settings.DEBUG)
 register_exception_handlers(app)
 
 app.include_router(health_router)
-# app.include_router(auth_router)
-# app.include_router(user_router)
+app.include_router(auth_router)
+app.include_router(user_router)
 # app.include_router(site_router)
 # app.include_router(work_item_router)
 
