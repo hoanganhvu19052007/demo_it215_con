8d55d5ed82316dd9c4b7ff6c8da7f63a7bc65b1c
hoanganhvu19052007 <hoangvu2007hg@gmail.com>
2026-08-21 02:02:26 +0700

    Delete construction_management directory

--- construction_management/app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-"""SQLAlchemy models."""
-from app.models.user import User
-from app.models.site import ConstructionSite, SiteMember
-from app.models.work_item import WorkItem
-
-__all__ = ["User", "ConstructionSite", "SiteMember", "WorkItem"]
